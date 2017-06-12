31b6b3cf7295300a2935028bab58c7f13f572822
Matthias Vandermaesen <matthias@colada.be>
2017-06-12 11:20:29 +0200

    Changed: reverted back to update method instead of add method in Catmandu::Bag

--- lib/Catmandu/Store/Datahub/Bag.pm
+++ lib/Catmandu/Store/Datahub/Bag.pm
@@ -70,7 +70,7 @@ sub get {
 # Create a new record
 sub add {
     my ($self, $data) = @_;
-    return $self->api->add($data->{'_'});
+    return $self->api->update($data->{'id'}, $data->{'_'});
 }
 
 ##
